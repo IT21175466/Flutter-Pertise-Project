89177994daa1ff5f90c22d31b8ea1849195183c0
Chamath Harshana <it21175466@my.sliit.lk>
2023-08-14 20:08:06 +0530

    OTP Verification Page Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sexpertise/Frontend/ForgotPasswordScreen.dart';
+import 'package:sexpertise/Frontend/OTPVerificationScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ForgotPasswordScreen(),
+      home: OTPVerificationScreen(),
     );
   }
 }
