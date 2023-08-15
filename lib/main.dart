6fb71cebc4e8298b30a6aca117288381772272a8
Chamath Harshana <it21175466@my.sliit.lk>
2023-08-15 07:45:14 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sexpertise/Frontend/SplashScreen.dart';
+import 'package:sexpertise/Interfaces/SplashScreen.dart';
 
 void main() {
   runApp(const MyApp());
