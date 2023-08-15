d09ed2620335d68ecbb1e99c29db275a5fdd6284
Chamath Harshana <it21175466@my.sliit.lk>
2023-08-15 10:54:00 +0530

    Firebase Removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sexpertise/Interfaces/SplashScreen.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
+void main() {
   runApp(const MyApp());
 }
 
