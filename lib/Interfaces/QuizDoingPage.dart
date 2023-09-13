df259d9216130440beba0edd65132f00ef086cb4
Chamath <chamathharshana989@gmail.com>
2023-09-13 19:24:47 +0530

    Quiz Clickable Done

--- lib/Interfaces/QuizDoingPage.dart
+++ lib/Interfaces/QuizDoingPage.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class QuizDoingPage extends StatefulWidget {
   final String quizID;
@@ -10,6 +14,109 @@ class QuizDoingPage extends StatefulWidget {
 }
 
 class _QuizDoingPageState extends State<QuizDoingPage> {
+  @override
+  void initState() {
+    getData(widget.quizID);
+    super.initState();
+  }
+
+  bool loading = false;
+
+  String question = '';
+  String answer01 = '';
+  String answer02 = '';
+  String answer03 = '';
+  String answer04 = '';
+  String correctAnswer = '';
+
+  void showCorrectDialog() {
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+        context: context,
+        builder: (BuildContext dialogContext) {
+          return CupertinoAlertDialog(
+            content: Column(
+              children: [
+                Container(
+                  child: Lottie.network(
+                      'https://assets7.lottiefiles.com/packages/lf20_9aa9jkxv.json'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text("Your Anser is Correct!"),
+              ],
+            ),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                child: Text("OK"),
+                onPressed: () {
+                  Navigator.of(dialogContext).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext dialogContext) {
+          return AlertDialog(
+            content: Container(
+              height: 305,
+              child: Column(
+                children: [
+                  Container(
+                    child: Lottie.network(
+                        'https://assets7.lottiefiles.com/packages/lf20_9aa9jkxv.json'),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("Your Anser is Correct!"),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(dialogContext).pop();
+                },
+                child: const Text('OK'),
+              ),
+
+              // child: Text("OK"),
+              // onPressed: () {
+              //   Navigator.of(dialogContext).pop();
+              // },
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  void getData(String uId) async {
+    setState(() {
+      loading = true;
+    });
+    final DocumentSnapshot questionDoc =
+        await FirebaseFirestore.instance.collection("Questions").doc(uId).get();
+
+    setState(() {
+      question = questionDoc.get('Question');
+      answer01 = questionDoc.get('Answer_01');
+      answer02 = questionDoc.get('Answer_02');
+      answer03 = questionDoc.get('Answer_03');
+      answer04 = questionDoc.get('Answer_04');
+      correctAnswer = questionDoc.get('Correct_Answer');
+    });
+    setState(() {
+      loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +147,7 @@ class _QuizDoingPageState extends State<QuizDoingPage> {
             Align(
               alignment: Alignment.topLeft,
               child: Text(
-                'loadedState.question[counter].question',
+                '$question',
                 style: TextStyle(
                   fontWeight: FontWeight.w600,
                   fontSize: 23,
@@ -49,6 +156,14 @@ class _QuizDoingPageState extends State<QuizDoingPage> {
             ),
             Spacer(),
             GestureDetector(
+              onTap: () {
+                if (answer01 == correctAnswer) {
+                  showCorrectDialog();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Your Answer is Wrong!")));
+                }
+              },
               child: Container(
                 height: 80,
                 width: double.infinity,
@@ -60,7 +175,7 @@ class _QuizDoingPageState extends State<QuizDoingPage> {
                 child: Align(
                   alignment: Alignment.centerLeft,
                   child: Text(
-                    'loadedState.question[counter].optionOne',
+                    '$answer01',
                     style: TextStyle(
                       fontWeight: FontWeight.w500,
                       fontSize: 17,
@@ -73,18 +188,29 @@ class _QuizDoingPageState extends State<QuizDoingPage> {
               height: 10,
             ),
             GestureDetector(
+              onTap: () {
+                if (answer02 == correctAnswer) {
+                  showCorrectDialog();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Your Answer is Wrong!")));
+                }
+              },
               child: Container(
                 height: 80,
                 width: double.infinity,
                 padding: EdgeInsets.symmetric(horizontal: 10),
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.white,
-                ),
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Color.fromRGBO(0, 74, 173, 1),
+                      width: 1.5,
+                    )),
                 child: Align(
                   alignment: Alignment.centerLeft,
                   child: Text(
-                    'loadedState.question[counter].optionTwo',
+                    '$answer02',
                     style: TextStyle(
                       fontWeight: FontWeight.w500,
                       fontSize: 17,
@@ -97,6 +223,14 @@ class _QuizDoingPageState extends State<QuizDoingPage> {
               height: 10,
             ),
             GestureDetector(
+              onTap: () {
+                if (answer03 == correctAnswer) {
+                  showCorrectDialog();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Your Answer is Wrong!")));
+                }
+              },
               child: Container(
                 height: 80,
                 width: double.infinity,
@@ -108,7 +242,7 @@ class _QuizDoingPageState extends State<QuizDoingPage> {
                 child: Align(
                   alignment: Alignment.centerLeft,
                   child: Text(
-                    'loadedState.question[counter].optionThree',
+                    '$answer03',
                     style: TextStyle(
                       fontWeight: FontWeight.w500,
                       fontSize: 17,
@@ -121,6 +255,14 @@ class _QuizDoingPageState extends State<QuizDoingPage> {
               height: 10,
             ),
             GestureDetector(
+              onTap: () {
+                if (answer04 == correctAnswer) {
+                  showCorrectDialog();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Your Answer is Wrong!")));
+                }
+              },
               child: Container(
                 height: 80,
                 width: double.infinity,
@@ -132,7 +274,7 @@ class _QuizDoingPageState extends State<QuizDoingPage> {
                 child: Align(
                   alignment: Alignment.centerLeft,
                   child: Text(
-                    'loadedState.question[counter].optionFour',
+                    '$answer04',
                     style: TextStyle(
                       fontWeight: FontWeight.w500,
                       fontSize: 17,
@@ -144,7 +286,6 @@ class _QuizDoingPageState extends State<QuizDoingPage> {
             Spacer(),
           ],
         ),
-        //QuestionTile(questionModel: loadedState.question[0]),
       ),
     );
   }
