0b08ff9257425cd616ac997606dc2a3f9a394dcd
Chamath <chamathharshana989@gmail.com>
2023-09-16 00:03:05 +0530

    Update QuizEditPage.dart

--- lib/Interfaces/QuizEditPage.dart
+++ lib/Interfaces/QuizEditPage.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:sexpertise/Interfaces/AddQuestion.dart';
 import 'package:sexpertise/Interfaces/AdminQuizList.dart';
 
 class QuizEditPage extends StatefulWidget {
