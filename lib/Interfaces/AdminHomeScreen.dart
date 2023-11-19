98e660e30a0bc9002ee582d25f570346e31e4916
Chamath <chamathharshana989@gmail.com>
2023-11-19 19:34:04 +0530

    Update AdminHomeScreen.dart

--- lib/Interfaces/AdminHomeScreen.dart
+++ lib/Interfaces/AdminHomeScreen.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:sexpertise/Interfaces/Admin/ArticleListPage.dart';
+import 'package:sexpertise/Interfaces/Admin/Blog%20Function/ArticleListPage.dart';
 import 'package:sexpertise/Interfaces/AdminQuizList.dart';
 
 class AdminHomeScreen extends StatefulWidget {
